0b705f81b0b3de101c2b0a0d0d3c346463d8f677
arzzuu <arzzuu>
2022-11-25 19:23:02 +0400

    finish

--- script.js
+++ script.js
@@ -10,14 +10,26 @@ const in1 = document.getElementById('in1');
 const in2 = document.getElementById('in2');
 document.getElementById('in1').value = 1;
 
+enter1.addEventListener('input', updateValue1);
+enter.addEventListener('input', updateValue);
+
+function updateValue1() {
+  if(enter1.value[0] == '0' && enter1.value[1] == '0')
+  enter1.value = '0';
+}
+function updateValue() {
+  if(enter.value[0] == '0' && enter.value[1] == '0')
+  enter.value = '0';
+}
+
 fetch(`https://api.exchangerate.host/latest?base=${firstCurrency.value}`)
 .then((response) => response.json())
 .then((data) => {
     // console.log(data);
-    const response = data.rates[secondCurrency.value];
-    firstDiv.innerText = `1 ${firstCurrency.value} = ${response.toFixed(4)} ${secondCurrency.value}`;
-    secondDiv.innerText = `1 ${secondCurrency.value} = ${Number(1/response).toFixed(4)} ${firstCurrency.value}`;
-    secondVal.value = (firstVal.value * response).toFixed(4);
+    const res = data.rates[secondCurrency.value];
+    firstDiv.innerText = `1 ${firstCurrency.value} = ${res.toFixed(4)} ${secondCurrency.value}`;
+    secondDiv.innerText = `1 ${secondCurrency.value} = ${Number(1/res).toFixed(4)} ${firstCurrency.value}`;
+    secondVal.value = (firstVal.value * res).toFixed(4);
   })      .catch((error) => firstDiv.innerText = `Произошла ошибка: ${error.message}` );
 
   function numberWithSpaces(x) {
@@ -26,55 +38,47 @@ fetch(`https://api.exchangerate.host/latest?base=${firstCurrency.value}`)
     return parts.join(".");
   }
 
-  enter1.addEventListener('input', updateValue1);
-  enter.addEventListener('input', updateValue);
-  function updateValue1() {
-    if(enter1.value[0] == '0' && enter1.value[1] == '0')
-    enter1.value = '';
-  }
-
-  function updateValue() {
-    if(enter.value[0] == '0' && enter.value[1] == '0')
-    enter.value = '';
-  }
 
   $('input#in1').on('input', function() {
-  $(this).val($(this).val().replace(/\,/g, '.'))
-  $(this).val($(this).val().replace(/(?=(\d+\.\d{100})).+|(\.(?=\.))|([^\.\d])|(^\D)/gi, '$1'))
-  // $(this).val($(this).val().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 '))
-  })
-
-  $('input#in2').on('input', function() {
-  $(this).val($(this).val().replace(/\,/g, '.'))
-  $(this).val($(this).val().replace(/(?=(\d+\.\d{100})).+|(\.(?=\.))|([^\.\d])|(^\D)/gi, '$1'))
-  // $(this).val($(this).val().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1 '))
+      this.value = this.value.replace(/[^\d\.,]/g, "");
+      this.value = this.value.replace(/,/g, ".");
+      if(this.value.match(/\./g).length > 1) {
+          this.value = this.value.substr(0, this.value.lastIndexOf("."));
+      }
   });
 
-    function currencyExchange(firstCurrency, secondCurrency, firstVal, secondVal) {
-      if ((firstVal.value) == '' ) return
-    firstVal.value = numberWithSpaces(firstVal.value);
-    fetch(`https://api.exchangerate.host/latest?base=${firstCurrency.value}`)
-    .then((response) => response.json())
-    .then((data) => {
-        // console.log(data);
-        const response = data.rates[secondCurrency.value];
-        firstDiv.innerText = `1 ${firstCurrency.value} = ${response.toFixed(4)} ${secondCurrency.value}`;
-        secondDiv.innerText = `1 ${secondCurrency.value} = ${Number((1/response)).toFixed(4)} ${firstCurrency.value}`;
-        secondVal.value = numberWithSpaces((firstVal.value.split(' ').join('') * response).toFixed(4));
-      })
-      .catch((error) => firstDiv.innerText = `Произошла ошибка: ${error.message}` );
-    } 
- firstCurrency.addEventListener('change', function() {
-  currencyExchange(firstCurrency, secondCurrency, firstVal, secondVal);
+  $('input#in2').on('input', function() {
+    this.value = this.value.replace(/[^\d\.,]/g, "");
+    this.value = this.value.replace(/,/g, ".");
+    if(this.value.match(/\./g).length > 1) {
+        this.value = this.value.substr(0, this.value.lastIndexOf("."));
+    }
+  }); 
+  function currencyExchange(firstCurrency, secondCurrency, firstVal, secondVal) {
+    if ((firstVal.value) == '' ) return
+  firstVal.value = numberWithSpaces(firstVal.value);
+  fetch(`https://api.exchangerate.host/latest?base=${firstCurrency.value}`)
+  .then((response) => response.json())
+  .then((data) => {
+      // console.log(data);
+      const res = data.rates[secondCurrency.value];
+      firstDiv.innerText = `1 ${firstCurrency.value} = ${res.toFixed(4)} ${secondCurrency.value}`;
+      secondDiv.innerText = `1 ${secondCurrency.value} = ${Number((1/res)).toFixed(4)} ${firstCurrency.value}`;
+      secondVal.value = numberWithSpaces((firstVal.value.split(' ').join('') * res).toFixed(4));
+    })
+    .catch((error) => firstDiv.innerText = `Произошла ошибка: ${error.message}` );
+  } 
+firstCurrency.addEventListener('change', function() {
+currencyExchange(firstCurrency, secondCurrency, firstVal, secondVal);
 });
 secondCurrency.addEventListener('change', function() {
-    currencyExchange(firstCurrency, secondCurrency, secondVal, firstVal);
+  currencyExchange(firstCurrency, secondCurrency, secondVal, firstVal);
 });
 firstVal.addEventListener('input', function() {
-  currencyExchange(firstCurrency, secondCurrency, firstVal, secondVal);
+currencyExchange(firstCurrency, secondCurrency, firstVal, secondVal);
 });
 secondVal.addEventListener('input', function() {
-  currencyExchange(secondCurrency, firstCurrency, secondVal, firstVal);
+currencyExchange(firstCurrency, secondCurrency, secondVal, firstVal);
 });
     //   document.getElementById("sel").onchange = function(){
     //     if (this.prev) this.prev.style.color = "black";
